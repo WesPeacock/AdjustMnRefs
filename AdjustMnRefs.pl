965bed15497a1c37e35008f1511b365e89591664
Wes <wes_peacock@sil.org>
2023-07-14 11:36:23 -0600

    Process options and Config file

--- AdjustMnRefs.pl
+++ AdjustMnRefs.pl
@@ -1,15 +1,15 @@
 #!/usr/bin/env perl
 my $USAGE = "Usage: $0 [--inifile inifile.ini] [--section section] [--recmark lx] [--eolrep #] [--reptag __hash__] [--debug] [file.sfm]\n";
 =pod
-This script is a stub that provides the code for opl'ing and de_opl'ing an input file
-It also includes code to:
-	- use an ini file (commented out)
-	- process command line options including debugging
+This script is to adjust the main references and flag their corresponding subentry.
 
 The ini file should have sections with syntax like this:
-[section]
-Param1=Value1
-Param2=Value2
+[AdjustMnRefs]
+RecordMarker=lx
+SubentryMarkers=se
+MainREfMarker=mn
+SecondMainRefMarker=mnx
+SubentryMarkerSuffix=_ref
 
 =cut
 use 5.020;
@@ -28,7 +28,7 @@ my $scriptname = fileparse($0, qr/\.[^.]*/); # script name without the .pl
 use Getopt::Long;
 GetOptions (
 	'inifile:s'   => \(my $inifilename = "$scriptname.ini"), # ini filename
-	'section:s'   => \(my $inisection = "section"), # section of ini file to use
+	'section:s'   => \(my $inisection = "AdjustMnRefs"), # section of ini file to use
 # additional options go here.
 # 'sampleoption:s' => \(my $sampleoption = "optiondefault"),
 	'recmark:s' => \(my $recmark = "lx"), # record marker, default lx
@@ -42,20 +42,36 @@ GetOptions (
 	) or die $USAGE;
 
 # check your options and assign their information to variables here
-$recmark =~ s/[\\ ]//g; # no backslashes or spaces in record marker
 
-# if you do not need a config file ucomment the following and modify it for the initialization you need.
-# if you have set the $inifilename & $inisection in the options, you only need to set the parameter variables according to the parameter names
-# =pod
 use Config::Tiny;
 my $config = Config::Tiny->read($inifilename, 'crlf');
 die "Quitting: couldn't find the INI file $inifilename\n$USAGE\n" if !$config;
-my $param1 = $config->{"$inisection"}->{Param1};
-say STDERR "Param1:$param1" if $debug;
-my $param2 = $config->{"$inisection"}->{Param2};
-say STDERR "Param2:$param2" if $debug;
-# =cut
 
+$recmark = $config->{"$inisection"}->{RecordMarker} if $config->{"$inisection"}->{RecordMarker};
+$recmark = clean_marks($recmark); # no backslashes or spaces in record marker
+
+my $semarks = "se";
+$semarks = $config->{"$inisection"}->{SubentryMarkers} if $config->{"$inisection"}->{SubentryMarkers};
+$semarks = clean_marks($semarks);
+
+my $mnrefmark = "mn";
+$mnrefmark = $config->{"$inisection"}->{MainRefMarker} if $config->{"$inisection"}->{MainRefMarker};
+$mnrefmark = clean_marks($mnrefmark);
+
+my $altmnrefmark = "mnx";
+$altmnrefmark = $config->{"$inisection"}->{AltMainRefMarker} if $config->{"$inisection"}->{AltMainRefMarker};
+$altmnrefmark =clean_marks($altmnrefmark);
+
+my $altsubsuffix = "_ref";
+$altsubsuffix = $config->{"$inisection"}->{AltSubentrySuffix} if $config->{"$inisection"}->{AltSubentrySuffix};
+
+say STDERR "Record marker:$recmark" if $debug;
+say STDERR "Subentry marker:$semarks" if $debug;
+say STDERR "Main Ref marker:$mnrefmark" if $debug;
+say STDERR "Alternate Main Ref marker:$altmnrefmark" if $debug;
+say STDERR "Alternate Subentry Marker Suffix:$altsubsuffix" if $debug;
+
+# die "after options/Config";
 # generate array of the input file with one SFM record per line (opl)
 my @opledfile_in;
 my $line = ""; # accumulated SFM record
@@ -85,3 +101,15 @@ say STDERR "oplline:", Dumper($oplline) if $debug;
 		print;
 		}
 	}
+
+sub clean_marks {
+# converts an SFM list into a search string
+my ($marks) = @_;
+for ($marks) {
+	s/\\//g;
+	s/ //g;
+	s/\,*$//; # no trailing commas
+	s/\,/\|/g;  # use bars for or'ing
+	}
+return $marks;
+}
